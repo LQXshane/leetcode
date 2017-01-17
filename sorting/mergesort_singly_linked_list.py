07494847ec314d9c5486fc829768d8c532e69cc1
LQXshane <qxlin@bu.edu>
2017-01-17 12:41:24 -0500

    clear solution

--- sorting/mergesort_singly_linked_list.py
+++ sorting/mergesort_singly_linked_list.py
@@ -1,35 +1,39 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution(object):
+    # merge sort, recursively
     def sortList(self, head):
-    #     """
-    #     :type head: ListNode
-    #     :rtype: ListNode
-    #     """
-        if not head or not head.next: return head
-        
-        prev, slow, fast = None, head, head
-        while fast and fast.next:
-            prev, slow, fast  = slow, slow.next, fast.next.next
-        prev.next = None
-        
-        return self.mergeList(*map(self.sortList, (head, slow)))
-        
+        if not head or not head.next:
+            return head
+        # divide list into two parts
+        fast, slow = head.next, head
 
-    def mergeList(self, l1, l2):
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        # the reason why this cut-into-half works:
+        # init: slow and fast are neighbors
+        # 1st loop: slow moves 1, fast moves 2 steps, they are two steps away
+        # 2nd loop: 3 steps away
+        # 3rd loop: 4 steps away
+        # ...
+        # the loop will break when fast pointer reaches the end of this list
+        second = slow.next
+        # cut down the first part, using slow is legit because
+        # in python you slow = head(like in init) allows the access of "slow" to the address of this list,
+        # we will be making changes to the list then
+        slow.next = None
+        l = self.sortList(head)
+        r = self.sortList(second)
+        return self.merge(l, r)
+
+    def merge(self, l, r):
+        # both l and r are sorted lists
         dummy = tail = ListNode(None)
-        while l1 and l2: #both l1 and l2 are sorted lists
-            if l1.val < l2.val:
-                tail.next, tail, l1 = l1, l1, l1.next
+        while l and r:
+            if l.val < r.val:
+                tail.next, tail, l = l, l, l.next
             else:
-                tail.next, tail, l2 = l2, l2, l2.next
-                
+                tail.next, tail, r = r, r, r.next
+
         # if l1 or l2 does not have more than one elements, break the loop
-        tail.next = l1 or l2
+        tail.next = l or r
         return dummy.next
-                
-            
