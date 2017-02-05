fca153bd8e582e55857a9cc6125900d16d473367
LQXshane <qxlin@bu.edu>
2017-02-05 16:51:05 -0500

    pre order added

--- tree/traversal.py
+++ tree/traversal.py
@@ -42,3 +42,28 @@ class Solution(object):
                 ans.append(tmp.val)
                 root = tmp.right
         return ans
+
+# 2. Preorder
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        
+        # remember Stack property: LIFO
+        
+        res, stack = [], [root]
+        
+        while stack:
+            
+            cur = stack.pop()
+            
+            if cur:
+                
+                res.append(cur.val)
+                stack.append(cur.right)
+                stack.append(cur.left)
+                
+        return res
